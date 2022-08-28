c1adb4784e4b4162be8916aafea77bb17de135e0
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-08-28 15:30:17 -0300

    esqueleto

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    BannerComponent,
+    AcercadeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    FooterComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
