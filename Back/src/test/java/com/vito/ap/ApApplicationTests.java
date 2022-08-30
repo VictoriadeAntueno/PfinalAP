b8a38a7f850f5db981925004456f19fca6a2e2a7
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-08-30 08:21:45 -0300

    Back+FrontStyle

---
+++ Back/src/test/java/com/vito/ap/ApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vito.ap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
